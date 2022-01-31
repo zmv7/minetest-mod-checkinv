b23a3e19f6548120423fc412d38df8df6192a4cb
zmv7 <72821250+zmv7@users.noreply.github.com>
2022-02-01 04:41:18 +0500

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,20 @@
+minetest.register_chatcommand("pinv", {
+  description = "View online player's inventory",
+  params = "<playername>",
+  privs = {pinv=true},
+  func = function(name, param)
+local detached_inv = minetest.create_detached_inventory(param,{
+  allow_move = function()
+    	return 0
+	end,
+	allow_put = function()
+    	return 0
+	end,
+	allow_take = function()
+		return 0
+	end,})
+local player_inv = minetest.get_inventory({type='player', name = param})
+if not player_inv then return false else
+detached_inv:set_list('main', player_inv:get_list('main'))
+minetest.show_formspec(name,'pinv','size[8,4]'..'list[detached:'..param..';main;0,0;8,4;]')
+end end,})

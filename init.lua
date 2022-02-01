39d820e0eea6cf81a9b1fae074b9e4fc9e616afd
zmv7 <72821250+zmv7@users.noreply.github.com>
2022-02-01 15:42:18 +0500

    Added label and 'close' button

--- init.lua
+++ init.lua
@@ -17,5 +17,5 @@ local detached_inv = minetest.create_detached_inventory(param,{
 local player_inv = minetest.get_inventory({type='player', name = param})
 if not player_inv then return false else
 detached_inv:set_list('main', player_inv:get_list('main'))
-minetest.show_formspec(name,'pinv','size[8,4]'..'list[detached:'..param..';main;0,0;8,4;]')
+minetest.show_formspec(name,'pinv','size[8,5]label[0,0;'..param..'`s inventory]'..'list[detached:'..param..';main;0,0.5;8,4;]button_exit[3,4.7;2,0.5;;Close]')
 end end,})
